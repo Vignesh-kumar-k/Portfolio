387942e5481c21b01fe970fae50fce9235ce7a9b
Vignesh-kumar-k <vigneshvicky84321@gmail.com>
2024-02-04 09:59:41 +0530

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+export default function Header(){
+    return <header className="flex justify-between">
+        <a href="">Vignesh Kumar</a>
+        <ul className="flex">
+           <li><a href="">Home</a></li>
+           <li><a href="">About</a></li>
+           <li><a href="">Project</a></li>
+           <li><a href="">Contact</a></li>
+        </ul>
+    </header>
+}
