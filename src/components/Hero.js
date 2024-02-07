4b468592857c89edc57a8f4ecaf9d00f47036b27
Vignesh-kumar-k <vigneshvicky84321@gmail.com>
2024-02-07 19:55:49 +0530

    1

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,11 @@
 import Heroimg from "../assets/hero.png"
 export default function Hero(){
     return <section className="flex px-5 py-32 bg-secondary justify-center">
-        <h1 className="w-1/2  text-white text-4xl font-hero-font">HI <br/> Im vignesh
-            <p className="text-2xl">A passionate developer always learning.</p>
-        </h1>
-       
+        <div className="w-1/2">
+            <h1 className="text-white text-6xl font-hero-font">HI <br/> Im vignesh
+                <p className="text-2xl">A passionate developer always learning.</p>
+            </h1>
+        </div>
         <img className='w-1/3' src={Heroimg}/>
     </section>
 }
