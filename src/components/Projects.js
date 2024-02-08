0d67c6c99680d88bc7eb39b471b62d44764e60e8
Vignesh-kumar-k <vigneshvicky84321@gmail.com>
2024-02-08 20:05:26 +0530

    images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,28 @@
+import websiteimg1 from "../assets/img1.png"
+import websiteimg2 from "../assets/aiimg.png"
+import websiteimg3 from "../assets/website-blog.jpg"
 export default function Projects(){
-    return <section className="flex px-5 py-5 justify-center bg-primary text-white">
+    return <section className="flex flex-col px-5 py-20 justify-center bg-primary text-white">
         <div className="w-1/2">
-            <h1 className="text-4xl border-b-4 w-[125px] border-[#2b2d77] text-bold">Projects</h1>
+            <div className="flex justify-center"> 
+                <h1 className="text-4xl border-b-4 w-[128px] border-[#2b2d77] text-bold ">Projects</h1>
+            </div>
         </div>
-        <div className="w-1/2">
-            Images
+        <div className=" py-5 w-1/2">
+            <div className="flex px-10 gap-5">
+            <div>
+                <img src={websiteimg1} />
+                <div>
+                    <p>An E-commerce website built with MERN Stack</p>
+                </div>
+            </div>
+            <div>
+                <img src={websiteimg2} />
+            </div>
+            <div>
+                <img src={websiteimg3} />
+            </div>
+            </div>
         </div>
     </section>
 }
