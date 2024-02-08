87b9d8a8e06a26f0d5ab871fb690a384f46ddc69
Vignesh-kumar-k <vigneshvicky84321@gmail.com>
2024-02-08 19:29:03 +0530

    projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,9 @@
 export default function Projects(){
-    return <section className="flex px-5 py-5">
-        <div>
-            Heading
+    return <section className="flex px-5 py-5 justify-center bg-primary text-white">
+        <div className="w-1/2">
+            <h1 className="text-4xl border-b-4 w-[125px] border-[#2b2d77] text-bold">Projects</h1>
         </div>
-        <div>
+        <div className="w-1/2">
             Images
         </div>
     </section>
